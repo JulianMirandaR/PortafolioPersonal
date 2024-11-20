2b8fb6297ebf4786d3428ea4c3fa0fb922d0afdc
JulianMirandaR <juulimiranda@outlook.com>
2024-11-20 11:24:32 -0300

    Cambios para la entrega

--- function.js
+++ function.js
@@ -2,3 +2,18 @@ function enviar(){
     console.log("Su mensaje ha sido enviado a Julian Miranda")
     alert("Su mensaje ha sido enviado a Julian Miranda")
 }
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href'); // Obtiene el id del destino
+      const targetSection = document.querySelector(targetId);
+  
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop, // Calcula la posición del destino
+          behavior: 'smooth' // Desplazamiento suave
+        });
+      }
+    });
+  });
